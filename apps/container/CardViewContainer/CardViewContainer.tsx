96c813f86ca1a5f0b043c9e2cd40ab3c3ee7897f
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-03-07 20:04:57 +0900

    delete- cardViewContainer

--- apps/container/CardViewContainer/CardViewContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import CardView from '../../components/CardView';
-import TwitStore from '../../store/TwitStore';
-
-const CardViewContainer = () => {
-  return <CardView />;
-};
-
-export default CardViewContainer;
